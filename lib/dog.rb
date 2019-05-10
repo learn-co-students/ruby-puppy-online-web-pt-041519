36e60dc4699ed6783b284c198b4fbecc95798313
aparkening <aaron.parkening@gmail.com>
2019-05-10 14:01:24 -0400

    Done. Dogs initiates w/ name and stores in @@all

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class Dog
+  attr_accessor :name
+ 
+  @@all = []
+ 
+  # Initialize with name and store in @@all array
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  # Clear puppy database
+  def self.clear_all
+    @@all = []
+  end
+
+  # Print each puppy name
+  def self.all
+    @@all.each { |puppy| puts puppy.name}
+  end
+end
