c2a9dff76361607ac44c51a6beb53e2b2c102ae6
Caleb Aviles <earthctzn1@gmail.com>
2019-05-15 05:25:55 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+require 'pry'
+class Dog 
+  
+  attr_reader :name 
+  
+  @@all =[]
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each {|puppy| puts puppy.name}
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+    
+end
