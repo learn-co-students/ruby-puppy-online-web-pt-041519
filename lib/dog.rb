f3f327aa8a92765cebca86dc6a00ff4a8882fbff
Cody Frank <cody.frank30@gmail.com>
2019-05-18 01:48:50 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  @@all = []
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def self.clear_all
+    @@all = []
+  end
+  def self.all
+    @@all.each do |x|
+      puts x.name
+    end
+  end
+end
